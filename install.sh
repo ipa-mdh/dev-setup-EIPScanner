9b09d862df2d4fa27c6d61372bbf979b2d4d74eb
Max Daiber-Huppert <mdh@ipa.fhg.de>
2025-06-12 18:06:17 +0000

    remove conan

--- install.sh
+++ install.sh
@@ -2,25 +2,12 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-pip3 install conan
-
 PROJECT_FOLDER="EIPScanner"
 
 
 mkdir -p "${SCRIPT_DIR}/build"
 cd "${SCRIPT_DIR}/build"
 
-conan install .. --build=missing -s build_type=Release
-
-cmake "${SCRIPT_DIR}/${PROJECT_FOLDER}" \
-    -DCMAKE_BUILD_TYPE=Release \
-    -G "Unix Makefiles" \
-    -DCMAKE_TOOLCHAIN_FILE="${SCRIPT_DIR}/conan_toolchain.cmake"
-cmake --build . --config Release
-if [ $? -ne 0 ]; then
-    echo "Build failed. Please check the output for errors."
-    exit 1
-fi
-echo "Build completed successfully."
-
-cmake --install . --config Release
+cmake ..
+cmake --build . --target install
+make install
